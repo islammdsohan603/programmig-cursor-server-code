89ecb7efb4b0339dcfef1f0329511a1047877699
islammdsohan603 <islammdsohan603@gmail.com>
2026-05-21 19:50:10 +0600

    remove env fil

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT;
 
@@ -22,35 +23,29 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    await client.connect();
+    // await client.connect();
+
     const db = client.db("wanderlust");
 
     const cursorData = db.collection("cursor");
 
     app.get("/cursor", async (req, res) => {
-      const cursor = cursorData.find();
-      const result = await cursor.toArray();
+      const result = await cursorData.find().toArray();
+
       res.send(result);
     });
 
-    app.get(
-      "/cursor/:id",
+    app.get("/cursor/:id", async (req, res) => {
+      const id = req.params.id;
 
-      async (req, res) => {
-        const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-        const query = { _id: new ObjectId(id) };
+      const result = await cursorData.findOne(query);
 
-        const result = await cursorData.findOne(query);
+      res.send(result);
+    });
 
-        res.send(result);
-      },
-    );
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.log(error);
   }
@@ -58,10 +53,13 @@ const run = async () => {
 
 run();
 
-app.get("/", async (req, res) => {
-  res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send({
+    success: true,
+    message: "Server is running",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Prot is rouning now ${port}`);
+  console.log(`Port is running now ${port}`);
 });
